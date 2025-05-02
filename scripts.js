b7f6aeed2b87416585ba6596dcf649bee6fab068
Felipe Ferreira <fefucho08@gmail.com>
2025-05-02 14:30:55 -0700

    Started apps drawer

--- scripts.js
+++ scripts.js
@@ -1,13 +1,48 @@
 window.addEventListener("load", () => {
     const appsDrawer = document.querySelector(".appsDrawer");
-    const openDrawerIcon = document.querySelector(".appsIcon");
+    const openDrawerIcon = document.querySelector(".drawerIcon");
     const closeDrawerIcon = document.querySelector(".closeDrawer");
 
     openDrawerIcon.addEventListener("click", () => {
-        appsDrawer.classList.add("visible");
+        openDrawer();
     });
 
     closeDrawerIcon.addEventListener("click", () => {
-        appsDrawer.classList.remove("visible");
+        closeDrawer();
     });
+
+    function openDrawer() {
+        let id = null;
+        let translation = 100;
+        clearInterval(id);
+        appsDrawer.style.display = "block";
+        document.querySelector(".background").style.pointerEvents = "none";
+        const frame = () => {
+            if (translation == 0) {
+                clearInterval(id);
+            } else {
+                translation--;
+                appsDrawer.style.transform = `translateY(${translation}%)`;
+            }
+        };
+        id = setInterval(frame, 0.3);
+    }
+
+    function closeDrawer() {
+        let id = null;
+        let translation = 0;
+        clearInterval(id);
+        const frame = () => {
+            if (translation == 100) {
+                clearInterval(id);
+                appsDrawer.style.display = "none";
+                document.querySelector(".background").style.pointerEvents =
+                    "auto";
+            } else {
+                translation++;
+                appsDrawer.style.transform = `translateY(${translation}%)`;
+            }
+        };
+        id = setInterval(frame, 0.3);
+    }
 });
